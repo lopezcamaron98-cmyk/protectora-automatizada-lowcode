57d5775261d85a0a78d6c87f2ba31705301880e9
Laura López Camarón <lopezcamaron.98@gmail.com>
2026-09-18 02:17:53 +0200

    delete config

--- config.js
+++
@@ -1,6 +0,0 @@
-// config.js - ARCHIVO LOCAL CON CREDENCIALES
-// Este archivo contiene tus llaves reales para que funcione tu Live Server local.
-// config.js
-export const SUPABASE_URL = 'https://tdvvmusnlumltrctausl.supabase.co';
-export const SUPABASE_ANON_KEY = 'sb_publishable_R59ii3WGhJoU8KVILWgCSQ_EWl7894w';
-
